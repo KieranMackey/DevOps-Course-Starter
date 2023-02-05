3a2cc308da7d593c29387057211588203d533caf
KieranMackey <69765305+KieranMackey@users.noreply.github.com>
2023-02-05 16:10:22 +0000

    Exercise 1 - Implement 'add' for todo list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import render_template
+from flask import request, redirect, url_for
+import todo_app.data.session_items
 
 from todo_app.flask_config import Config
 
@@ -8,4 +11,19 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    todo_items = todo_app.data.session_items.get_items()
+    return render_template('index.html', todo_items=todo_items)
+
+@app.route('/add', methods = ['POST', 'GET'])
+def add_item():
+    if request.method == 'POST':
+        new_todo = request.form.get('todo_item')
+        todo_app.data.session_items.add_item(new_todo)
+        return redirect(url_for('index'))
+    if request.method == 'GET':
+        return redirect(url_for('index'))
+
+@app.route('/read')
+def read():
+    todo_items = todo_app.data.session_items.get_items()
+    return render_template('index.html', todo_items=todo_items)
